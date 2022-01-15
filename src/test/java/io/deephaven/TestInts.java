4eaa8228667945053d13e6b2da11c70e4b05c3be
Corey Kosak <coreykosak@deephaven.io>
2022-01-14 22:10:14 -0500

    update

--- src/test/java/io/deephaven/TestInts.java
+++ src/test/java/io/deephaven/TestInts.java
@@ -4,19 +4,16 @@ import gnu.trove.list.array.*;
 import io.deephaven.csv.reading.CsvReader;
 import io.deephaven.csv.sinks.Sink;
 import io.deephaven.csv.sinks.SinkFactory;
-import io.deephaven.csv.sinks.Source;
 import io.deephaven.csv.util.CsvReaderException;
 import io.deephaven.csv.util.Renderer;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class TestInts {
@@ -27,19 +24,9 @@ public class TestInts {
         final InputStream bais = new ByteArrayInputStream(tns.text.getBytes(StandardCharsets.UTF_8));
 
         final CsvReader reader = new CsvReader();
-        final SinkFactory sf = SinkFactory.of(
-                null, null,
-                null, null,
-                MyIntSink::new, null,
-                null, null,
-                null, null,
-                null, null,
-                null, null,
-                null, null,
-                null, null,
-                null, null,
-                null);
-        reader.read(bais, sf);
+        final SinkFactory sf = makeMySinkFactory();
+        final CsvReader.Result result = reader.read(bais, sf);
+        final int[] data = ((ResultProvider<int[]>)result.columns()[0]).toResult();
     }
 
     public static TextAndNubbins buildTable(final Random rng, final int numRows, final int numCols) {
@@ -99,26 +86,26 @@ public class TestInts {
         }
     }
 
-    public interface ColumnProvider<TARRAY> {
-        Column<TARRAY> toColumn(final String columnName);
+    public interface ResultProvider<TARRAY> {
+        TARRAY toResult();
     }
 
-    private static abstract class MySinkBase<TCOLLECTION, TARRAY> implements Sink<TARRAY>, ColumnProvider<TARRAY> {
+    private static abstract class MySinkBase<TCOLLECTION, TARRAY> implements Sink<TARRAY>, ResultProvider<TARRAY> {
         protected final TCOLLECTION collection;
         protected int collectionSize;
         protected final FillOperation<TCOLLECTION> fillOperation;
         protected final SetOperation<TCOLLECTION, TARRAY> setOperation;
         protected final AddOperation<TCOLLECTION, TARRAY> addOperation;
-        protected final BiFunction<TCOLLECTION, String, Column<TARRAY>> toColumnOperation;
+        protected final Function<TCOLLECTION, TARRAY> toResultOperation;
 
         protected MySinkBase(TCOLLECTION collection, FillOperation<TCOLLECTION> fillOperation,
                              SetOperation<TCOLLECTION, TARRAY> setOperation, AddOperation<TCOLLECTION, TARRAY> addOperation,
-                             BiFunction<TCOLLECTION, String, Column<TARRAY>> toColumnOperation) {
+                             Function<TCOLLECTION, TARRAY> toResultOperation) {
             this.collection = collection;
             this.fillOperation = fillOperation;
             this.setOperation = setOperation;
             this.addOperation = addOperation;
-            this.toColumnOperation = toColumnOperation;
+            this.toResultOperation = toResultOperation;
         }
 
         @Override
@@ -130,7 +117,6 @@ public class TestInts {
             final int size = Math.toIntExact(destEnd - destBegin);
             final int destBeginAsInt = Math.toIntExact(destBegin);
             final int destEndAsInt = Math.toIntExact(destEnd);
-            nullFlagsToValues(isNull, src, size);
 
             if (!appending) {
                 // Replacing.
@@ -148,8 +134,8 @@ public class TestInts {
             collectionSize = destEndAsInt;
         }
 
-        public final Column<TARRAY> toColumn(final String columnName) {
-            return toColumnOperation.apply(collection, columnName);
+        public final TARRAY toResult() {
+            return toResultOperation.apply(collection);
         }
 
         /**
@@ -176,14 +162,14 @@ public class TestInts {
 
     private static abstract class MySourceAndSinkBase<TCOLLECTION, TARRAY> extends MySinkBase<TCOLLECTION, TARRAY>
             implements io.deephaven.csv.sinks.Source<TARRAY>, Sink<TARRAY> {
-        private final ToArrayOperation<TCOLLECTION, TARRAY> toArrayOperation;
+        private final ReadToArrayOperation<TCOLLECTION, TARRAY> readToArrayOperation;
 
         protected MySourceAndSinkBase(TCOLLECTION collection, FillOperation<TCOLLECTION> fillOperation,
                                       SetOperation<TCOLLECTION, TARRAY> setOperation, AddOperation<TCOLLECTION, TARRAY> addOperation,
-                                      BiFunction<TCOLLECTION, String, Column<TARRAY>> toColumnOperation,
-                                      ToArrayOperation<TCOLLECTION, TARRAY> toArrayOperation) {
-            super(collection, fillOperation, setOperation, addOperation, toColumnOperation);
-            this.toArrayOperation = toArrayOperation;
+                                      Function<TCOLLECTION, TARRAY> toResultOperation,
+                                      ReadToArrayOperation<TCOLLECTION, TARRAY> readToArrayOperation) {
+            super(collection, fillOperation, setOperation, addOperation, toResultOperation);
+            this.readToArrayOperation = readToArrayOperation;
         }
 
         @Override
@@ -192,35 +178,29 @@ public class TestInts {
                 return;
             }
             final int size = Math.toIntExact(srcEnd - srcBegin);
-            toArrayOperation.apply(collection, dest, Math.toIntExact(srcBegin), 0, size);
+            readToArrayOperation.apply(collection, dest, Math.toIntExact(srcBegin), 0, size);
         }
 
         /**
          * Meant to be paired with e.g. TDoubleArrayList.add(double[] dest, int source_pos, int dest_pos, int length)
          */
-        private interface ToArrayOperation<TCOLLECTION, TARRAY> {
+        private interface ReadToArrayOperation<TCOLLECTION, TARRAY> {
             void apply(TCOLLECTION coll, TARRAY dest, int source_pos_, int dest_pos, int length);
         }
     }
 
     private static class MyByteSinkBase extends MySourceAndSinkBase<TByteArrayList, byte[]> {
-        protected final byte nullSentinel;
-
-        public MyByteSinkBase(final byte nullSentinel, final Class<?> reinterpretedType) {
+        public MyByteSinkBase() {
             super(new TByteArrayList(),
                     (dest, from, to) -> dest.fill(from, to, (byte) 0),
                     TByteArrayList::set,
                     TByteArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()).reinterpret(reinterpretedType),
-                    TByteArrayList::toArray);
-            this.nullSentinel = nullSentinel;
+                    TByteArrayList::toArray,
+                    TByteArrayList::toArray);  // Note: different "toArray" from the above.
         }
     }
 
     private static final class MyByteSink extends MyByteSinkBase {
-        public MyByteSink() {
-            super(Sentinels.NULL_BYTE, byte.class);
-        }
     }
 
     private static final class MyShortSink extends MySourceAndSinkBase<TShortArrayList, short[]> {
@@ -229,8 +209,8 @@ public class TestInts {
                     (dest, from, to) -> dest.fill(from, to, (short) 0),
                     TShortArrayList::set,
                     TShortArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()),
-                    TShortArrayList::toArray);
+                    TShortArrayList::toArray,
+                    TShortArrayList::toArray);  // Note: different "toArray" from the above.
         }
     }
 
@@ -240,26 +220,23 @@ public class TestInts {
                     (dest, from, to) -> dest.fill(from, to, 0),
                     TIntArrayList::set,
                     TIntArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()),
-                    TIntArrayList::toArray);
+                    TIntArrayList::toArray,
+                    TIntArrayList::toArray);  // Note: different "toArray" from the above.
         }
     }
 
     private static class MyLongSinkBase extends MySourceAndSinkBase<TLongArrayList, long[]> {
-        public MyLongSinkBase(final Class<?> reinterpretedType) {
+        public MyLongSinkBase() {
             super(new TLongArrayList(),
                     (dest, from, to) -> dest.fill(from, to, 0L),
                     TLongArrayList::set,
                     TLongArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()).reinterpret(reinterpretedType),
-                    TLongArrayList::toArray);
+                    TLongArrayList::toArray,
+                    TLongArrayList::toArray);  // Note: different "toArray" from the above.
         }
     }
 
     private static final class MyLongSink extends MyLongSinkBase {
-        public MyLongSink() {
-            super(Sentinels.NULL_LONG, long.class);
-        }
     }
 
     private static final class MyFloatSink extends MySinkBase<TFloatArrayList, float[]> {
@@ -268,7 +245,7 @@ public class TestInts {
                     (dest, from, to) -> dest.fill(from, to, 0),
                     TFloatArrayList::set,
                     TFloatArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()));
+                    TFloatArrayList::toArray);
         }
     }
 
@@ -278,14 +255,11 @@ public class TestInts {
                     (dest, from, to) -> dest.fill(from, to, 0),
                     TDoubleArrayList::set,
                     TDoubleArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()));
+                    TDoubleArrayList::toArray);
         }
     }
 
     private static final class MyBooleanAsByteSink extends MyByteSinkBase {
-        public MyBooleanAsByteSink() {
-            super(Sentinels.NULL_BOOLEAN_AS_BYTE, boolean.class);
-        }
     }
 
     private static final class MyCharSink extends MySinkBase<TCharArrayList, char[]> {
@@ -294,7 +268,7 @@ public class TestInts {
                     (coll, from, to) -> coll.fill(from, to, (char) 0),
                     TCharArrayList::set,
                     TCharArrayList::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray()));
+                    TCharArrayList::toArray);
         }
     }
 
@@ -304,7 +278,7 @@ public class TestInts {
                     MyStringSink::fill,
                     MyStringSink::set,
                     MyStringSink::add,
-                    (dest, name) -> Column.ofArray(name, dest.toArray(new String[0])));
+                    c -> c.toArray(new String[0]));
         }
 
         private static void fill(final ArrayList<String> dest, final int from, final int to) {
@@ -333,15 +307,9 @@ public class TestInts {
     }
 
     private static final class MyDateTimeAsLongSink extends MyLongSinkBase {
-        public MyDateTimeAsLongSink() {
-            super(null, Instant.class);
-        }
     }
 
     private static final class MyTimestampAsLongSink extends MyLongSinkBase {
-        public MyTimestampAsLongSink() {
-            super(null, Instant.class);
-        }
     }
 
     private static SinkFactory makeMySinkFactory() {
