bd148870d6ab9ee34f2aba01524a954009a9e8f4
Corey Kosak <coreykosak@deephaven.io>
2022-01-15 20:18:48 -0500

    so painful

--- src/jmh/java/io/deephaven/Zamboni.java
+++ src/jmh/java/io/deephaven/Zamboni.java
@@ -1,22 +1,23 @@
 package io.deephaven;
 
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
 @State(Scope.Thread)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(value = 3,
-        jvmArgsAppend = {"-server", "-disablesystemassertions"})
-public class MyBenchmark {
-
+public class Zamboni {
     int x = 923;
     int y = 123;
 
-    @GenerateMicroBenchmark
+    @Benchmark
     @Warmup(iterations = 10, time = 3, timeUnit = TimeUnit.SECONDS)
     public int baseline() {
         return x;
     }
 
-    @GenerateMicroBenchmark
+    @Benchmark
     @Warmup(iterations = 5, time = 5, timeUnit = TimeUnit.SECONDS)
     public int sum() {
         return x + y;
